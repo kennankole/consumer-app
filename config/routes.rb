456033526a4a3e87f536f9d979158ef766834bd9
kennankole <kennankole@gmail.com>
2023-03-21 16:45:54 +0300

    Finished creating transaction page. Written all intergration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'entities/show'
-  get 'entities/index'
-  get 'entities/new'
-  get 'entities/create'
-  get 'entities/destroy'
+  # get 'entities/show'
+  # get 'entities/index'
+  # get 'entities/new'
+  # get 'entities/create'
+  # get 'entities/destroy'
   devise_for :users
   resources :users do
     resources :categories, only: [:new, :create, :edit, :update, :destroy, :index]
+    resources :entities, only: [:new, :create, :index]
   end
 
   get 'home/index'
--- spec/views/entities/new.html.erb_spec.rb
+++ spec/views/entities/new.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "entities/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "entities/new.html.erb", type: :feature do
+  user = User.create!(email: 'entity10@email.com', password: 'qwerty')
+  group1 = Group.create!(name: 'Docker', icon: 'urlhttps/img', user: user)
+  group2 = Group.create(name: 'DevOps', icon: 'urlhttps/img/img', user: user)
+
+  before do
+    login_as(user, user: :scope)
+    visit new_user_entity_path(user_id: user.id)
+  end
+
+  it 'Creates new Transaction' do
+    expect(page).to have_content('Add New Transaction')
+    fill_in 'Name', with: 'Transaction 1'
+    fill_in 'Amout', with: 20.50
+    select 'DevOps', from: 'entity_group_id'
+    click_on 'Add Transaction'
+    visit user_entities_path(user_id: user.id)
+    expect(page).to have_content('Find me in app/views/entities/index.html.erb')
+  end
 end
