c72b60357b196e53bf36a26ac46252907d420804
kennankole <kennankole@gmail.com>
2023-03-21 08:33:55 +0300

    Adding many-to-many relationship between entities and groups tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_320_093_306) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_053119) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'entities', force: :cascade do |t|
-    t.string 'name'
-    t.float 'amout'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_entities_on_user_id'
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.float "amout"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_groups_on_user_id'
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "entity_id", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  add_foreign_key 'entities', 'users'
-  add_foreign_key 'groups', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
--- spec/views/home/home.html.erb_spec.rb
+++ spec/views/home/home.html.erb_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :feature do
+  user = User.create!(email: 'users30@email.com', password: 'qwerty')
+  before do
+    login_as(user, scope: :user)
+  end
+
   scenario 'When a user visits the home page' do
     visit "/"
     expect(page).to have_content("Snapscan")
-    expect(page).to have_link('Log in')
-    expect(page).to have_link('Sign Up')
+    expect(page).to have_content('Log out')
+    expect(page).to have_button('Log out')
   end
 end
