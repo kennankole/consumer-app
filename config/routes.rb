22a672bfae52a3f74af89b9df40dbf9be1c18543
kennankole <kennankole@gmail.com>
2023-03-22 20:01:25 +0300

    Removing commented code from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # get 'entities/show'
-  # get 'entities/index'
-  # get 'entities/new'
-  # get 'entities/create'
-  # get 'entities/destroy'
   devise_for :users
   resources :users do
     resources :categories, only: [:new, :create, :edit, :update, :destroy, :index]
