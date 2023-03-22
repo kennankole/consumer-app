9dd20b0c4ed256abf73e9e9f7e6bfc6e9ee741cf
kennankole <kennankole@gmail.com>
2023-03-22 11:43:28 +0300

    Refactored categories new.html.erb_spec.rb

--- spec/views/categories/new.html.erb_spec.rb
+++ spec/views/categories/new.html.erb_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "categories/create.html.erb", type: :system do
-  user = User.create!(email: 'tom@me.com', password: 'passcode')
+  user = User.create!(email: 'timm@me.com', password: 'passcode')
   before do
     login_as(user, scope: :user)
-    visit new_user_category_path(user)
   end
 
   it 'It creates a new category' do
+    visit new_user_category_path(user)
     fill_in 'Name', with: 'School feees'
     fill_in 'Icon', with: 'https://img.freepik.com/free-photo/neon-tropical-monstera-leaf-banner_53876-138943.jpg?w=2000'
     click_on 'Save'
     visit user_categories_path(user_id: user.id)
-    expect(page).to have_http_status(:success)
+    expect(page.current_path).to eq(user_categories_path(user_id: user.id))
   end
 end
