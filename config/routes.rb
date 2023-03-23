0849b7efb71b6874dceb590beb2a00ffe0336621
kennankole <kennankole@gmail.com>
2023-03-23 14:59:02 +0300

    Refactoring the code base according to PR comments Modified entities_controller to perform according to project requirements Modified categories_controller to perform according to project requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users do
-    resources :categories, only: [:new, :create, :edit, :update, :destroy, :index]
-    resources :entities, only: [:new, :create, :index]
+    resources :categories, only: [:new, :create, :edit, :update, :destroy, :index] do
+      resources :entities, only: [:new, :create, :index]
+    end
+    # resources :entities, only: [:new, :create, :index]
   end
 
   get 'home/index'
   root to: 'home#index'
 end
-
